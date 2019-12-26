a399668ffdf1d8599a60a38475cacbcb7e21218d
Jeff Gibson <jeffgibson@agcode.com>
2019-12-26 13:40:05 -0600

    Initial setup

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseKeys = {
+  apiKey: 'AIzaSyCq9apnLh6tAo2fE638--zuTG7w0GNJxY0',
+  authDomain: 'anycms-bb4e0.firebaseapp.com',
+  databaseURL: 'https://anycms-bb4e0.firebaseio.com',
+  projectId: 'anycms-bb4e0',
+  storageBucket: 'anycms-bb4e0.appspot.com',
+  messagingSenderId: '294150749164',
+  appId: '1:294150749164:web:5db4838e20c228b6509ec9',
+  measurementId: 'G-69K5PVYG5K'
+}
